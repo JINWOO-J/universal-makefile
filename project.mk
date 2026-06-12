4af8b6847ae7286e4d7379a643ab34597001f11f
Jinwoo <jinwoo@parametacorp.com>
2026-06-12 14:09:19 +0900

    chore(release): bump version to v1.0.237

--- project.mk
+++ project.mk
@@ -1,6 +1,6 @@
 REPO_HUB=jinwoo
 NAME=universal-makefile
-VERSION = v1.0.236
+VERSION = v1.0.237
 # 이 repo의 main/develop 이력은 no-ff 머지 기반 — linear promote는 push 거부로 실패함
 RELEASE_MERGE_STYLE = merge
 IGNORE_TAG_RE = cache
